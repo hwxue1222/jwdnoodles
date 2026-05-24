83b7a77eb340bea00235a4e14b276f625c6407f5
Luke1222 <hwxue1222@gmail.com>
2026-05-24 11:59:44 +0800

    Align menu thumbnails from top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -571,7 +571,7 @@ export default function Home() {
                         className="snap-start shrink-0 w-[72vw] sm:w-[340px] rounded-xl border border-[#d5e6c3] bg-white/60 hover:bg-white transition overflow-hidden text-left"
                         onClick={() => openLightbox(p.src, p.label[lang])}
                       >
-                        <SafeImg src={p.src} alt={p.label[lang]} className="w-full h-36 object-cover" />
+                        <SafeImg src={p.src} alt={p.label[lang]} className="w-full h-36 object-cover object-top" />
                         <div className="px-3 py-2 text-xs text-[#486449]">{p.label[lang]}</div>
                       </button>
                     ))}
